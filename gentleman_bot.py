bd839dd0cca85d3a15080cf4187fc14d6a6f1e76
Nicolas K. Ribas <nicolaskribas@gmail.com>
2022-10-06 17:01:30 -0300

    Individual themes

--- gentleman_bot.py
+++ gentleman_bot.py
@@ -1,7 +1,8 @@
 import asyncio
 import logging
-import discord
 from os import path, getenv, makedirs
+
+import discord
 from discord import FFmpegOpusAudio
 from discord.ext import commands
 
@@ -9,10 +10,11 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='~', intents=intents)
+bot = commands.Bot(command_prefix="~", intents=intents)
 
 storage_dir = path.join(
-    getenv("XDG_DATA_HOME", path.expanduser("~/.local/share")), "gentleman-bot")
+    getenv("XDG_DATA_HOME", path.expanduser("~/.local/share")), "gentleman-bot"
+)
 
 
 @bot.command()
@@ -21,16 +23,37 @@ async def ping(ctx):
 
 
 @bot.command()
+async def info(ctx):
+    if ctx.message.mentions:
+        theme_path = path.join(
+            storage_dir, str(ctx.guild.id), str(ctx.message.mentions[0].id)
+        )
+    else:
+        theme_path = path.join(storage_dir, str(ctx.guild.id), "global")
+
+    if path.isfile(theme_path):
+        await ctx.reply("Theme setted")
+    else:
+        await ctx.reply("No theme setted")
+
+
+@bot.command()
 async def theme(ctx, attachment: discord.Attachment):
     # TODO: validate attachment
 
-    theme = path.join(storage_dir, str(ctx.guild.id), "global")
-    makedirs(path.dirname(theme), exist_ok=True)
-    await attachment.save(theme)
+    if ctx.message.mentions:
+        theme_path = path.join(
+            storage_dir, str(ctx.guild.id), str(ctx.message.mentions[0].id)
+        )
+    else:
+        theme_path = path.join(storage_dir, str(ctx.guild.id), "global")
+
+    makedirs(path.dirname(theme_path), exist_ok=True)
+    await attachment.save(theme_path)
 
 
 @bot.event
-async def on_command_error(ctx, err):
+async def on_command_error(_ctx, err):
     if isinstance(err, commands.CommandNotFound):
         return
 
@@ -43,31 +66,34 @@ async def on_voice_state_update(member, before, after):
         return
 
     if is_voice(after.channel) and after.channel != before.channel:
-        theme = path.join(storage_dir, str(member.guild.id), "global")
-        if path.isfile(theme):
+        theme_path = path.join(storage_dir, str(member.guild.id), str(member.id))
+        if path.isfile(theme_path):
             voice = await join(after.channel)
-            voice.play(FFmpegOpusAudio(theme),
-                       after=disconnect(voice))
+            voice.play(FFmpegOpusAudio(theme_path), after=disconnect(voice))
+        else:
+            theme_path = path.join(storage_dir, str(member.guild.id), "global")
+            if path.isfile(theme_path):
+                voice = await join(after.channel)
+                voice.play(FFmpegOpusAudio(theme_path), after=disconnect(voice))
 
 
 async def join(channel):
-    if (voice := discord.utils.get(bot.voice_clients, guild=channel.guild)) != None:
-        if voice.is_playing():
-            voice.pause()
+    if (voice := discord.utils.get(bot.voice_clients, guild=channel.guild)) is not None:
+        voice.pause()
         await voice.move_to(channel)
         return voice
-    else:
-        return await channel.connect(timeout=2.0)
+
+    return await channel.connect(timeout=2.0)
 
 
 def is_voice(channel):
-    return channel != None and not isinstance(channel, discord.StageChannel)
+    return channel is not None and not isinstance(channel, discord.StageChannel)
 
 
 # https://discordpy.readthedocs.io/en/stable/faq.html#how-do-i-pass-a-coroutine-to-the-player-s-after-function
 def disconnect(voice):
     def voice_disconnect(play_err):
-        if play_err != None:
+        if play_err is not None:
             logging.warning(play_err)
 
         asyncio.run_coroutine_threadsafe(voice.disconnect(), bot.loop)
